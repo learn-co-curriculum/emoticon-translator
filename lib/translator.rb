545b691a84b2f851b41e5b79a5057c2d3a7923d8
Katie Hoffman <ktahoffman@gmail.com>
2014-09-22 18:19:28 -0400

    add some code for loading yml

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,9 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  YAML.load_file(file_path)
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def translate
+  library = load_library("./lib/emoticons.yml")
 end
