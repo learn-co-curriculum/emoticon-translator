f83b98d158d338dbe6546be1990cbccde4658d40
Katie Hoffman <ktahoffman@gmail.com>
2014-09-22 18:36:16 -0400

    passes all tests

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,21 @@
 require 'yaml'
+require 'pry'
 
 def load_library(file_path)
-  YAML.load_file(file_path)
+  library = {}
+  YAML.load_file(file_path).each do |_str, array|
+    english, japanese = array
+    library[english] = japanese
+  end
+  library
 end
 
-def translate
+def translate(emoticon)
   library = load_library("./lib/emoticons.yml")
+  result = library[emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
