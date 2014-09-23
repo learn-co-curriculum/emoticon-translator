6aa68dfea8fe2b2633a1930e60bc0f988f071382
Katie Hoffman <ktahoffman@gmail.com>
2014-09-22 20:53:39 -0400

    passing all specs now that last two methods take two args

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,17 +2,28 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  library = {}
-  YAML.load_file(file_path).each do |_str, array|
+  library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file_path).each do |meaning, array|
     english, japanese = array
-    library[english] = japanese
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
   end
   library
 end
 
-def translate(emoticon)
-  library = load_library("./lib/emoticons.yml")
-  result = library[emoticon]
+def get_japanese_emoticon(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
+
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_meaning"][emoticon]
   if result
     result
   else
